d203572e38afc68720444581371a4aa5f9f76eb2
Egidijus <noneede@gmail.com>
2019-04-21 04:16:35 +0000

    cleared trash files

--- src/webpack.js
+++ src/webpack.js
@@ -1,16 +1,13 @@
 // import SassPlugin from '@pawjs/sass/webpack';
-import SrcsetPlugin from '@pawjs/srcset/webpack';
-import ImageOptimizer from '@pawjs/image-optimizer/webpack';
+// import SrcsetPlugin from '@pawjs/srcset/webpack';
+// import ImageOptimizer from '@pawjs/image-optimizer/webpack';
 import { PwaFucssPlugin } from 'next-fucss';
 // import SassPlugin from '@pawjs/pwa-sass/webpack';
 
 export default class ProjectWebpack {
   constructor(config) {
-    // console.log(config);
-    config.test = true;
     const { addPlugin } = config;
     // Add sass compiler to the project
-    // addPlugin(new SassPlugin());
     addPlugin(new PwaFucssPlugin({
       styleFile: '/src/resources/css/style.css'
     }));
@@ -19,8 +16,8 @@ export default class ProjectWebpack {
       supportedEnv: [ 'production' ],
       configLabel: 'MEDIUM_QUALITY',
     };
-    addPlugin(new ImageOptimizer(optimizerOptions));
+    // addPlugin(new ImageOptimizer(optimizerOptions));
 
-    addPlugin(new SrcsetPlugin());
+    // addPlugin(new SrcsetPlugin());
   }
 }
